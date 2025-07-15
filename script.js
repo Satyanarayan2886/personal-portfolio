c62cd38d7041604c39684fc99c14368ccb892fc2
Satyanarayan2886 <83575827+Satyanarayan2886@users.noreply.github.com>
2025-07-15 14:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,9 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+const cursor = document.getElementById("customCursor");
+
+document.addEventListener("mousemove", e => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
